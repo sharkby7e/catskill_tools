00a90ad7a6cba415bc4a3d9cdfed748e5ee18f7d
sharkby7e <sgquin@gmail.com>
2024-10-06 20:47:28 -0400

    setup sendgrid for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,12 +19,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # mail stuff
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: %w[ -i ]
-  # }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = { from: 'no-reply@hudsonvalleytools.com' }
