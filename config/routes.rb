39f6eda1ac09ea41d42b0e3692e525af7dc25e75
sharkby7e <sgquin@gmail.com>
2024-11-10 00:59:39 -0500

    Devise and turbo don't play well, so let's just turn it off for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'home#show'
