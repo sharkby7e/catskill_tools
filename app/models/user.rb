f8f7126515a18ebdda75e1d75c174bde81702a08
sharkby7e <sgquin@gmail.com>
2024-11-12 17:03:44 -0500

    figure out dependent destroy on another ticket

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable,
          :trackable, :omniauthable
 
-  has_many :tools, dependent: :destroy
+  has_many :tools
 end
 
 # == Schema Information
